7629b33be445f0bf4db5507573d7948de9409465
ahmetsefakara <ahmetsefakara@outlook.com>
2020-08-19 10:51:05 +0300

    Project created

---
+++ ApachePOIAndSeleniumTestNG/src/main/java/com/ask/ApachePOIAndSeleniumTestNG/App.java
@@ -0,0 +1,13 @@
+package com.ask.ApachePOIAndSeleniumTestNG;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
